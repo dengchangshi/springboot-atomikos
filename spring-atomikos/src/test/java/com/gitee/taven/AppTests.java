b44946944a646991c86e88d72fffd3395f51c555
邓常识 <dengcs1@hua-cloud.com.cn>
2021-04-23 11:59:58 +0800

    第二次提交

--- spring-atomikos/src/test/java/com/gitee/taven/AppTests.java
+++ spring-atomikos/src/test/java/com/gitee/taven/AppTests.java
@@ -1,23 +1,28 @@
 package com.gitee.taven;
 
+import com.gitee.taven.service.TestService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.gitee.taven.service.UserService;
-
+/**
+ * ClassName: 类名
+ * Description: 描述信息
+ *
+ * @Author: dengchangshi
+ * @Date: 2021/4/23 11:46
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppTests {
 
-	@Autowired
-	private UserService userService;
-	
-	@Test
-	public void contextLoads() {
-		userService.testJTA();
-	}
+    @Autowired
+    private TestService testService;
 
+    @Test
+    public void contextLoads() {
+        testService.testJTA();
+    }
 }
